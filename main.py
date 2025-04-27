a33232463f499044f564c545c74ecbded4fe43da
Carter Temm <cartertemm@gmail.com>
2025-04-26 17:35:32 -0700

    Update main.py to use argparse for more convenient invocation

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import sys
+import argparse
 import json
+import sys
 from dictionary_api import word_data as wd
 
 
+VERSION = "1.0.0"
+# Dictionary of available functions and their corresponding implementations
 options_mapping = {
 	"plain": wd.get_word_data_plain,
 	"data": wd.get_word_data,
@@ -40,16 +43,11 @@ def menu(prompt, items, default=0):
 
 
 def run_interactively():
+	"""Runs the dictionary tool in interactive mode."""
 	options = list(options_mapping.keys())
-	options_str = ", ".join(options_mapping)
+	options_str = ", ".join(options)
 	print("Running in interactive mode.")
-	print(f"Typical usage: {sys.argv[0]} [function] <word>")
-	print(
-		"Available functions: {options_str}"
-	)
-	print()
-	print(f"Example: {sys.argv[0]} game")
-	print(f"Or {sys.argv[0]} definitions opportunity")
+	print(f"Available functions: {options_str}")
 	print()
 	selection = menu("Select an option: ", options)
 	option = options[selection]
@@ -57,26 +55,66 @@ def run_interactively():
 	return (option, word)
 
 
-def define(option, word):
+def define(option, word, as_json=False):
+	"""Retrieves the requested information about the word."""
 	result = options_mapping[option](word)
+	
+	if as_json:
+		return json.dumps(result, indent=4)
+	
 	if option != "plain":
 		if isinstance(result, list):
 			result = "\n".join([i.capitalize() for i in result])
-		else:
+		elif isinstance(result, dict):
 			result = json.dumps(result, indent=4)
+	
 	return result
 
 
+def parse_args():
+	"""Parse command line arguments using argparse."""
+	parser = argparse.ArgumentParser(
+		description="Dictionary tool to look up words and their properties.",
+		epilog="Example usage: dict.py happy --definitions"
+	)
+	parser.add_argument('--version', action='version', version=f'%(prog)s {VERSION}')
+	parser.add_argument('word', nargs='?', help='Word to look up')
+	function_group = parser.add_argument_group('word functions')
+	function_group.add_argument('-p', '--plain', action='store_const', const='plain', dest='function',
+							help='Get plain word data (default)')
+	function_group.add_argument('-d', '--data', action='store_const', const='data', dest='function',
+							help='Get full word data')
+	function_group.add_argument('--pos', action='store_const', const='pos', dest='function',
+							help='Get parts of speech')
+	function_group.add_argument('-s', '--synonyms', action='store_const', const='synonyms', dest='function',
+							help='Get synonyms')
+	function_group.add_argument('-a', '--antonyms', action='store_const', const='antonyms', dest='function',
+							help='Get antonyms')
+	function_group.add_argument('-D', '--definitions', action='store_const', const='definitions', dest='function',
+							help='Get definitions')
+	function_group.add_argument('-e', '--examples', action='store_const', const='examples', dest='function',
+							help='Get examples')
+	function_group.add_argument('--hypernyms', action='store_const', const='hypernyms', dest='function',
+							help='Get hypernyms')
+	function_group.add_argument('--hyponyms', action='store_const', const='hyponyms', dest='function',
+							help='Get hyponyms')
+	parser.add_argument('-j', '--json', action='store_true', help='Force JSON output format')
+	# Add interactive mode
+	parser.add_argument('-i', '--interactive', action='store_true', help='Run in interactive mode')
+	return parser.parse_args()
+
+
 def main():
-	if len(sys.argv) < 2:
+	"""Main entry point for the dictionary tool."""
+	args = parse_args()
+	if args.interactive or not args.word:
 		function, word = run_interactively()
-	elif len(sys.argv) == 2:
-		function = "plain"
-		word = sys.argv[1]
-	elif len(sys.argv) == 3:
-			function = sys.argv[1]
-			word = sys.argv[2]
-	print(define(function, word))
+	else:
+		word = args.word
+		# Default to 'plain' if no function is specified
+		function = args.function if args.function else 'plain'
+	result = define(function, word, args.json)
+	print(result)
 
 
 if __name__ == "__main__":
